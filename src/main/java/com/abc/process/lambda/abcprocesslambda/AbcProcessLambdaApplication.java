79f0caf18a99994e990f1c63de3ee248fb6aafc3
dorjee <dorjee9>
2024-03-01 19:53:08 -0700

    first commit

---
+++ src/main/java/com/abc/process/lambda/abcprocesslambda/AbcProcessLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.abc.process.lambda.abcprocesslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcProcessLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcProcessLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/process/lambda/abcprocesslambda/AbcProcessLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.process.lambda.abcprocesslambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcProcessLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
